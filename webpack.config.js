bc7fcc04febcbfe4e5a8a28076f41ccc92ce2224
cullenjett@gmail.com <cullenjett@gmail.com>
2016-12-23 14:39:01 -0700

    super basic webpack config with dev server refreshing on js and html changes

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: [
+    "./src/main.js",
+    "webpack-dev-server/client?http://localhost:8080"
+  ],
+  output: {
+    path: __dirname + "/dev",
+    publicPath: "http://localhost:8080/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015"]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    })
+  ],
+  devServer: {
+    contentBase: "./src",
+    historyApiFallback: true,
+    stats: 'minimal'
+ }
+}
